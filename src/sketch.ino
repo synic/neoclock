2a66acf6f4eb23d06bae61a2c20298632cb4e724
Adam Olsen <arolsen@gmail.com>
2014-03-15 09:35:00 -0600

    started repo

---
+++ src/sketch.ino
@@ -0,0 +1,76 @@
+#include <Adafruit_NeoPixel.h> 
+#include <Wire.h>
+#include "RTClib.h"
+
+ 
+RTC_DS1307 RTC;
+uint8_t PIXELS = 24;
+uint8_t NEO_PIN = 13;
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXELS, NEO_PIN, NEO_GRB + NEO_KHZ800);
+boolean first = true;
+ 
+void setup () {
+    Serial.begin(9600);
+    Wire.begin();
+    RTC.begin();
+    strip.begin();
+    strip.show();
+ 
+  if (! RTC.isrunning()) {
+    Serial.println("RTC is NOT running!");
+    // following line sets the RTC to the date & time this sketch was compiled
+    //RTC.adjust(DateTime(__DATE__, __TIME__));
+  }
+ 
+}
+
+void clearStrip() {
+  for(int i = 0; i < PIXELS; i++) {
+    strip.setPixelColor(i, 1, 0, 1);
+  }
+}
+ 
+void loop () {
+    DateTime now = RTC.now();  
+    if(first) {
+       uint8_t start = now.second();
+       while(now.second() == start) {
+         now = RTC.now();
+       }
+       first = false;
+    }
+  
+
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(' ');
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+    
+    clearStrip();
+
+    uint8_t value = now.minute();
+    uint8_t minutes = map(value, 0, 60, 0, PIXELS);
+    strip.setPixelColor((int)minutes, 0, 80, 0);
+    
+    value = now.hour() % 12;
+    uint8_t hours = value * 2;
+    strip.setPixelColor((int)hours, 80, 0, 0);
+    
+    strip.show();
+    delay(200);
+    
+    value = now.second();
+    uint8_t seconds = map(value, 0, 60, 0, PIXELS);
+    strip.setPixelColor((int)seconds, 50, 0, 80);
+    strip.show();
+
+    delay(800);
+}
