e38cc7ddd70ad547b25cc8f5ad0bf9f5dd57cc19
Adam Olsen <arolsen@gmail.com>
2014-04-04 09:41:38 -0600

    color blending woo

--- src/sketch.ino
+++ src/sketch.ino
@@ -5,18 +5,30 @@
 #include "RTClib.h"
 #include "RoundClock.h"
 
-// constants
 const uint8_t PIXELS = 24;
 const uint8_t NEO_PIN = 13;
-const unsigned int SYNC_MAX = 18000; // 5 hours
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXELS, 
+    NEO_PIN, NEO_GRB + NEO_KHZ800);
+
+const uint8_t SYNC_MAX = 18000; // 5 hours
+const uint8_t FADE_PAUSE = 65;
+
+// colors
+const uint32_t OFF_COLOR = strip.Color(1, 0, 1);
+const uint32_t MINUTES_COLOR = strip.Color(0, 20, 0);  
+const uint32_t HOURS_COLOR = strip.Color(10, 0, 0);
+
+// number of LEDs for each hand
+const uint32_t MINUTES_LEDS = 2;
+const uint32_t HOURS_LEDS = 1;
+const uint32_t SECONDS_LEDS = 6;
 
 // other variables
 RTC_DS1307 RTC;
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXELS, 
-    NEO_PIN, NEO_GRB + NEO_KHZ800);
 boolean syncLoop = true;
-unsigned int loopCount = 0;
-RoundClock *clock = new RoundClock();
+uint8_t loopCount = 0;
+RoundClock clock = RoundClock();
+uint8_t *currentColors = new uint8_t[PIXELS] {0};
  
 void setup () {
     Serial.begin(9600);
@@ -26,22 +38,22 @@ void setup () {
     strip.show();
  
     if (! RTC.isrunning()) {
-        Serial.println("RTC is NOT running!");
         // following line sets the RTC to the date & time this 
         // sketch was compiled
         RTC.adjust(DateTime(__DATE__, __TIME__));
     }
 
-    for(int i = 0; i < PIXELS; i++) {
-        clock->add(i);
+    for(uint8_t i = 0; i < PIXELS; i++) {
+        clock.add(i);
     }
     
-    clock->done();
+    clock.done();
 }
 
 void clearStrip() {
-    for(int i = 0; i < PIXELS; i++) {
-        strip.setPixelColor(i, 1, 0, 1);
+    for(uint8_t i = 0; i < PIXELS; i++) {
+        currentColors[i] = OFF_COLOR;
+        strip.setPixelColor(i, OFF_COLOR);
     }
 }
 
@@ -51,21 +63,41 @@ long mapRange(double x, double in_min, double in_max, double out_min,
         out_min);
 }
 
-void fadeIn(int start, int count, int endPercent) {
-    int add = (float)endPercent / count;
-    Serial.print("add: ");
-    Serial.println(add);
-    int brightness = add;
+/**
+ * Blends two colors.
+ * 
+ * This code was borroed from Kevin Alford - https://github.com/zeroeth
+ */
+uint32_t blend(uint32_t color1, uint32_t color2) {
+    uint8_t r1, g1, b1;
+    uint8_t r2, g2, b2;
 
-    int first = true;
+    r1 = (uint8_t)(color1 >> 16);
+    g1 = (uint8_t)(color1 >> 8);
+    b1 = (uint8_t)(color1 >> 0);
+
+    r2 = (uint8_t)(color2 >> 16);
+    g2 = (uint8_t)(color2 >> 8);
+    b2 = (uint8_t)(color2 >> 0);
+
+    return strip.Color(constrain(r1 + r2, 0, 255),
+        constrain(g1 + g2, 0, 255), constrain(b1 + b2, 0, 255));
+}
+
+void fadeIn(uint8_t start, uint8_t count, uint8_t _end) {
+    uint8_t add = (float)_end / count;
+    uint8_t brightness = add;
+
+    uint8_t first = true;
     while(count > 0) {
-        int led = clock->back(start, count);
-        Serial.println(brightness);
-        strip.setPixelColor(led, brightness / 2, 0, brightness);
+        uint8_t led = clock.back(start, count);
+        uint32_t color = strip.Color(brightness / 2, 0, brightness);
+        
+        setColor(led, color);
 
         if(!first) {
-            strip.setPixelColor(clock->back(led, 2), 1, 0, 1);
-            if(count == 1) strip.setPixelColor(clock->back(led, 1), 1, 0, 1);
+            setColor(clock.back(led, 2), OFF_COLOR);
+            if(count == 1) setColor(clock.back(led, 1), OFF_COLOR);
         }
         strip.show();
         delay(35);
@@ -74,6 +106,17 @@ void fadeIn(int start, int count, int endPercent) {
         first = false;
     }
 }
+
+void setColor(uint8_t led, uint32_t color) {
+    if(color != OFF_COLOR) {
+        if(currentColors[led] != OFF_COLOR) {
+            color = blend(currentColors[led], color);
+        }
+    }
+
+    currentColors[led] = color;
+    strip.setPixelColor(led, color);
+} 
  
 void loop () {
     DateTime now = RTC.now();  
@@ -91,23 +134,28 @@ void loop () {
     clearStrip();
 
     uint8_t value = now.minute();
-    long minutes = mapRange(value, 0, 60, 0, PIXELS);
-    long start = minutes;
+    uint8_t minutes = mapRange(value, 0, 60, 0, PIXELS);
+    uint8_t start = minutes;
 
-    for(int i = 0; i < 2; i++) {
-        strip.setPixelColor(minutes, 0, 20, 0);
-        minutes = clock->back(start, i + 1);
+    for(uint8_t i = 0; i < MINUTES_LEDS; i++) {
+        setColor(minutes, MINUTES_COLOR);
+        minutes = clock.back(start, i + 1);
     }
 
     value = now.hour() % 12;
-    int hours = round(value * 2);
-    strip.setPixelColor(hours, 10, 0, 0);
+    uint8_t hours = round(value * 2);
+    start = hours;
+
+    for(uint8_t i = 0; i < HOURS_LEDS; i++) { 
+        setColor(hours, HOURS_COLOR);
+        hours = clock.back(start, i + 1); 
+    }
     
     strip.show();
     delay(200);
     
     value = now.second();
-    long seconds = mapRange(value, 0, 60, 0, PIXELS);
+    uint8_t seconds = mapRange(value, 0, 60, 0, PIXELS);
     fadeIn(seconds, 12, 100);
 
     delay(1000 - 200 - 12 * 35);
